481a9601ad069ca4c0c1e34175284a9008ada007
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-01-28 16:15:17 -0500

    Exercise 11 - Variables Comparison

---
+++ Exercise_11/exercise11.js
@@ -0,0 +1,23 @@
+/*  10 < 9
+    45 == 34
+    true === 1
+    false === 0
+    1 == 0
+    4 <= 2
+    4 >= 8  */
+
+    10 < 11
+    45 == 45
+    true === true
+    false === false
+    1 == true   // 1, "1", '1'
+    4 <= 8 
+    4 >= 2
+
+    console.log(10 < 11);
+    console.log(45 == 45);
+    console.log(true === true);
+    console.log(false === false);
+    console.log(1 == true);
+    console.log(4 <= 8);
+    console.log(4 >= 2);
