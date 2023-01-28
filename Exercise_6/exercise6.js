e8904dae839205a1ee97aadca9cb85bb95367da0
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-01-28 05:30:25 -0500

    Exercise 6 - Type Conversion

---
+++ Exercise_6/exercise6.js
@@ -0,0 +1,13 @@
+let hello = 'Ciao';     // boolean
+let age = 18;       // string
+let isGraduated = false;     // number
+
+
+hello = Boolean(hello);     // true
+age = String(age);      // 18   but this time it's a string :) 
+isGraduated = Number(isGraduated);      // 0
+
+
+console.log(hello);     
+console.log(age);         
+console.log(isGraduated);       
