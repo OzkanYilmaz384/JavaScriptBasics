869b9a2192822b5083e539770e375571e91a8812
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-01-29 15:51:56 -0500

    Exercise 16 - Conditional Operator

---
+++ Exercise_16/exercise16.js
@@ -0,0 +1,5 @@
+let isSunnyDay = true;
+let isHomeworkCompleted = false;
+
+let doesGoOut = isSunnyDay && isHomeworkCompleted ? "Jesse can go out to play" : "Jesse stays home";
+console.log(doesGoOut);
