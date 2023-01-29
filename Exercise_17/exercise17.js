72218bc761a9799a996a540843f7f8056f1ddcc6
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-01-29 17:12:06 -0500

    Exercise 17 - If/else statement

---
+++ Exercise_17/exercise17.js
@@ -0,0 +1,11 @@
+let age = 17;
+
+let ageToDrive;
+
+if (age >= 18) {
+    ageToDrive = "You can drive a car";
+}  else { 
+    ageToDrive = "You are too young to drive";
+}
+
+console.log(ageToDrive);
