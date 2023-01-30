9919d675e28e71b07661f6f4013e6d719e7cfe72
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-01-30 01:31:22 -0500

    Exercise 20 - If/else statement

---
+++ Exercise_20/exercise20.js
@@ -0,0 +1,14 @@
+let primitive = false;
+
+if (typeof(primitive) == "number") {
+    console.log(typeof(primitive));
+
+} else if (typeof(primitive) == "string") {
+    console.log(typeof(primitive));
+
+} else if (typeof(primitive) == "boolean") {
+    console.log(typeof(primitive));
+
+} else {
+    console.log("Please assign a value to 'primitive'")
+}
