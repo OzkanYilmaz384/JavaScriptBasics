172f75621353c7001de68bcd126663b3a9039160
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-01-30 08:57:07 -0500

    Exercise 25 - Loops

---
+++ Exercise_25/exercise25.js
@@ -0,0 +1,11 @@
+for (let number = 0; number >= -10; number--) {
+    console.log(number);
+}
+
+/*
+let withWhile = 0;
+while (withWhile >= -10) {
+    console.log(withWhile);
+    withWhile--;
+}
+*/
