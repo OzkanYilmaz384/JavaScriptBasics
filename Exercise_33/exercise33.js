d69d18c4b3161e5e36d5708c4381067f085d7557
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-01-31 06:51:59 -0500

    Exercise 33 - Arrow Functions

---
+++ Exercise_33/exercise33.js
@@ -0,0 +1,18 @@
+/*function sum() {
+    return 5 + 5;
+    }
+    console.log(sum());*/
+
+   
+    // This is my first method for Arrow Function
+    let arrowFunctionSum = (arg1, arg2) => {
+        return arg1 + arg2;
+    }
+
+    console.log(arrowFunctionSum(5, 5));
+
+    
+    // OR we can do this with ANOTHER METHOD for Arrow Function :
+    let arrowFuncSum = () => 5 + 5;
+    
+    console.log(arrowFuncSum());
