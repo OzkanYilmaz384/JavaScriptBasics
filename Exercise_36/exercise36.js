14045303a4a186a01d9e3759cc11d1eacd849083
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-02-01 08:14:18 -0500

    Exercise 36 - Scope

--- Exercise_36/exercise36.js
+++ Exercise_36/exercise36.js
@@ -1,11 +1,12 @@
-let sport = " Football";  //global sco
+let sport = " Football";  //global scope
 
 function canPlay() {
     
     if (true) {
     var personName = "Cosimo";
     }
-    
+    // We can define personName with 'var' instead of 'let' or 
+    // we can move the "console.log(personName + sport)" inside if statement to solve the problem.
     console.log(personName + sport);
     }
     
