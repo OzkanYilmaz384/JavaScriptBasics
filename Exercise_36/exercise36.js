33e112519019ef5e0df5545a876349f76e644bbb
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-02-01 08:01:03 -0500

    Exercise 36 - Scope

---
+++ Exercise_36/exercise36.js
@@ -0,0 +1,12 @@
+let sport = " Football";  //global sco
+
+function canPlay() {
+    
+    if (true) {
+    var personName = "Cosimo";
+    }
+    
+    console.log(personName + sport);
+    }
+    
+    canPlay();
