8c6a2e834c730ac50c26a706e2e9227348fb818e
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-02-02 12:41:11 -0500

    Exercise 48 - Array

---
+++ Exercise_48/exercise48.js
@@ -0,0 +1,7 @@
+const students = [
+    {id : 101 , name : "Name1", surname : "Surname1", age : 10},
+    {id : 202 , name : "Name2", surname : "Surname2", age : 11},
+    {id : 303 , name : "Name3", surname : "Surname3", age : 12}
+];
+
+console.log(students[0]);
