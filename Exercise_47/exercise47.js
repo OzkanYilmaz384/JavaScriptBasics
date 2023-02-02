9f31512867f85b94127573eb567c5a394e690077
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-02-02 11:38:26 -0500

    Exercise 47 - Array

---
+++ Exercise_47/exercise47.js
@@ -0,0 +1,6 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers[0]);        //first element of the array.
+console.log(numbers.length);    //the array length
+console.log(numbers[numbers.length - 1]); // console.log(9); // the last element of the array
+console.log(numbers[20]);   //the twentieth element of the array doesn't exist. So, it's undefined.
