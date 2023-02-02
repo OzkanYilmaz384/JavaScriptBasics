31e260ddbf0f528c91ff8044ed8ec6f9eedf3d63
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-02-02 04:19:47 -0500

    Exercise 46 - Nested Objects

---
+++ Exercise_46/exercise46.js
@@ -0,0 +1,14 @@
+const { emitWarning } = require("process");
+
+const student = {
+    id: 1,
+    school: "Liceo",
+    year: 3,
+    personalDate: {
+        name: "Emir",
+        surname: "Yilmaz",
+        age: 8,
+    }
+    };
+
+    console.log(student.personalDate);
