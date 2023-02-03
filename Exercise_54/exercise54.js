706440dcbb40bbcf98672e212a25cca8b764547a
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-02-03 15:21:03 -0500

    Exercise 54 - Array Methods Advanced

--- Exercise_54/exercise54.js
+++ Exercise_54/exercise54.js
@@ -23,3 +23,28 @@ function nicknameMap(persons) {
   
   console.log(persons);
   nicknameMap(persons);
+
+  /////////////////////     THIRD METHOD    //////////////////////////////////////////////
+/*
+  function nicknameMap(persons) {
+    let mapped = persons.map((nick) => nick.name + "-" + nick.age);
+    return mapped;
+  }
+  
+  const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+  ];
+  
+  const nicknames = nicknameMap(persons);
+  console.log(persons);
+  console.log(nicknames);
+  */
