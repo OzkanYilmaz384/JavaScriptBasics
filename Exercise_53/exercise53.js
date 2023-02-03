cd41dfc4f44641fd51993005947762d4c3174ae1
Ozkan Yilmaz <ozkanyilmaz384@gmail.com>
2023-02-03 13:04:17 -0500

    Exercise 53 - Array Methods Advanced

---
+++ Exercise_53/exercise53.js
@@ -0,0 +1,36 @@
+ function adultFilter(persons) {
+    let filtered = [];
+    for (let item of persons){
+        if (item.age >=18) {
+            filtered.push(item);
+        }
+    }
+    return filtered;
+  } 
+/*
+function adultFilter(persons) {
+    let filtered = [];
+    for (i = 0 ; i < persons.length ; i++){
+        if (persons[i].age >=18) {
+            filtered.push(persons[i]);
+        }
+    }
+    return filtered;
+  }
+*/
+const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+  ];
+  
+  const adults = adultFilter(persons);
+  console.log(persons);
+  console.log(adults);
